2a2baf7a2e2fa230b76c82ccf39cd7e533158ad8
LeandroVandari <bernardokreutz@gmail.com>
2025-05-21 22:11:52 -0300

    slightly simplified

--- timeline/src/time/mod.rs
+++ timeline/src/time/mod.rs
@@ -2,14 +2,12 @@ pub struct StandardCalendar {
     days_from_reference_date: i128,
 }
 
-pub trait Calendar
+pub trait Calendar: for<'any> From<&'any StandardCalendar>
 where
-    Self: for<'any> From<&'any StandardCalendar>,
-    for<'any> &'any Self: Into<StandardCalendar>,
+    StandardCalendar: for<'any> From<&'any Self>,
 {
     fn convert_to<T: Calendar>(&self) -> T
     where
-        T: for<'any> From<&'any StandardCalendar>,
         StandardCalendar: for<'any> From<&'any T>,
     {
         (&<&Self as Into<StandardCalendar>>::into(self)).into()
